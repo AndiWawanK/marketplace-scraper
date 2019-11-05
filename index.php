5bde346479ca918a9fbaff0a2e8e179da874d725
Andi WawanK <andifirwansyah250@gmail.com>
2019-11-05 07:37:50 +0700

    update index.php

--- index.php
+++ index.php
@@ -6,7 +6,6 @@ require_once __DIR__."/src/Marketplaces/Shopee.php";
 require_once __DIR__."/src/Marketplaces/Blanja.php";
 require_once __DIR__ . "/src/DB/Db.php";
 
-
 use Marketplaces\Bukalapak;
 use Marketplaces\Shopee;
 use Marketplaces\Blanja;
@@ -19,10 +18,17 @@ $bukalapak = new Bukalapak("https://www.bukalapak.com/u/rikky");
 $store_info = $bukalapak->getStoreInformation();
 $store_products = $bukalapak->getProducts();
 
-// $db = new DB("202.47.80.110", "root", "mplacesisfor84!", "esmart");
+$db = new DB('localhost:8889','esmart','root','root');
 
-// $id_toko = $db->insertStore($store_info);
+$id_toko = $db->insertStore($store_info);
 
+$insert_product = $db->insertProducts($store_products, $id_toko);
+// if($id_toko){
+//     echo "Insert Store Information Success";
+
+// }else{
+//     echo "Failed to Insert Store Information";
+// }
 
 // // End clock time in seconds
 $end_time = microtime(true);
@@ -32,11 +38,11 @@ $execution_time = ($end_time - $start_time);
 
 echo " Execution time of script = " . $execution_time . " sec";
 
-$store_file = fopen("store.txt", "w");
-fwrite($store_file,print_r($store_info,true));
+// $store_file = fopen("store.txt", "w");
+// fwrite($store_file,print_r($store_info,true));
 
-$product_file = fopen("products.txt", "w");
-fwrite($product_file, print_r($store_products, true));
+// $product_file = fopen("products.txt", "w");
+// fwrite($product_file, print_r($store_products, true));
 
-$debug_file = fopen("debug.txt","w");
-fwrite($debug_file," Execution time of script = " . $execution_time . " sec");
+// $debug_file = fopen("debug.txt","w");
+// fwrite($debug_file," Execution time of script = " . $execution_time . " sec");
