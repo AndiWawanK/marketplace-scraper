42c57953a4d7ee6871eef31d7218ddd38cb3fbee
Andi WawanK <andifirwansyah250@gmail.com>
2019-11-05 07:39:59 +0700

    update db & add some query to insert store information & product

--- src/DB/Db.php
+++ src/DB/Db.php
@@ -1,65 +1,120 @@
 <?php
 
-class DB
-{
-    private $db;
+class DB{
+    // private $db;
 
-    public function __construct($host, $user, $pass, $db)
-    {
+    public function __construct($host,$db, $user, $pass){
 
         try {
-            $this->db = new PDO("mysql:host=" . $host . ";dbname=" . $db, $user, $pass);
+            $this->db = new PDO("mysql:host=".$host.";dbname=".$db, $user, $pass);
         } catch (PDOException $e) {
             echo 'Connection failed: ' . $e->getMessage();
         }
+    
     }
 
     public function insertStore($data)
     {
-        $id_perusahan = substr(str_replace(".", "", microtime(true)), 8);
+        // $create_id_perusahaan = substr(str_replace(".", "", microtime(true)), 10);
+        // $id_perusahan = (int)$create_id_perusahaan;
+        // $id_perusahan = "";
 
-        $sql = "INSERT INTO `perusahaan`(
-                 `id_perusahaan`,
-                 `id_marketplace`,
-                 `nama`,
-                 `nama_toko`,
-                 `keterangan`,
-                 `alamat`,
-                 `foto`,
-                 `tgl_daftar`
+        $getid = "SELECT * FROM perusahaan ORDER BY id_perusahaan DESC LIMIT 1";
+        $resultId = $this->db->query($getid);
+        $resultsId = $resultId->fetchAll(PDO::FETCH_ASSOC);
+
+        $id_perusahan = $resultsId[0]['id_perusahaan'];
+
+        if($resultsId == NULL){
+            $id_perusahan = 1;
+            $sql = "INSERT INTO `perusahaan` (
+                `id_perusahaan`, 
+                `id_marketplace`, 
+                `nama`, 
+                `nama_toko`, 
+                `keterangan`, 
+                `alamat`, 
+                `foto`, 
+                `tgl_daftar`
                 ) VALUES (
-                    " . $id_perusahan . ",
-                    1,
-                    \"" . $data['name'] . "\",
-                    \"" . $data['shopname'] . "\",
-                    '" . $data['description'] . "',
-                    \"" . $data['location'] . "\",
-                    \"" . $data['shop_image'] . "\",
-                    \"" . $data['join_date'] . "\"
+                    $id_perusahan, 
+                    1, 
+                    '$data[name]',
+                    '$data[shopname]',
+                    '$data[description]',
+                    '$data[location]',
+                    '$data[shop_image]',
+                    '$data[join_date]'
                 )";
+             $this->db->query($sql);
+        }else{
+            $id_perusahan = $resultsId[0]['id_perusahaan'] + 1;
+            $sql = "INSERT INTO `perusahaan` (
+                `id_perusahaan`, 
+                `id_marketplace`, 
+                `nama`, 
+                `nama_toko`, 
+                `keterangan`, 
+                `alamat`, 
+                `foto`, 
+                `tgl_daftar`
+                ) VALUES (
+                    $id_perusahan, 
+                    1, 
+                    '$data[name]',
+                    '$data[shopname]',
+                    '$data[description]',
+                    '$data[location]',
+                    '$data[shop_image]',
+                    '$data[join_date]'
+                )";
+             $this->db->query($sql);
 
-        $this->db->query($sql);
+        }
 
         //    var_dump( $this->db->errorInfo());
+        $getLastIdToko = "SELECT * FROM toko ORDER BY id_toko DESC LIMIT 1";
+        $idToko = $this->db->query($getLastIdToko);
+        $resultIdToko = $idToko->fetchAll(PDO::FETCH_ASSOC);
 
-        $id_toko = substr(str_replace(".", "", microtime(true)), 8);
+        $id_toko = $resultIdToko[0]['id_toko'];
 
-        $sql = "INSERT INTO `toko`(
-            `id_toko`,
-            `id_perusahaan`,
-            `id_marketplace`,
-            `url`,
-            `tgl_daftar`
-            ) VALUES (
-                " . $id_toko . ",
-                " . $id_perusahan . ",
-                1,
-                \"" . $data['url'] . "\",
-                \"" . $data['join_date'] . "\"
-            )";
-
-        $this->db->query($sql);
-
+        if($resultIdToko == NULL){
+            $id_toko = 1;
+            
+            $sql = "INSERT INTO `toko`(
+                `id_toko`,
+                `id_perusahaan`,
+                `id_marketplace`,
+                `url`,
+                `tgl_daftar`
+                ) VALUES (
+                    $id_toko,
+                    $id_perusahan,
+                    1, 
+                    '$data[url]',
+                    '$data[join_date]'
+                )";
+        
+            $this->db->query($sql);
+        }else{
+            $id_toko = $resultIdToko[0]['id_toko'] + 1;
+            $sql = "INSERT INTO `toko`(
+                `id_toko`,
+                `id_perusahaan`,
+                `id_marketplace`,
+                `url`,
+                `tgl_daftar`
+                ) VALUES (
+                    $id_toko,
+                    $id_perusahan,
+                    1, 
+                    '$data[url]',
+                    '$data[join_date]'
+                )";
+    
+            $this->db->query($sql);
+        }
         return $id_toko;
     }
 
@@ -73,42 +128,122 @@ class DB
     public function insertProduct($data,$id_toko)
     {
         
-        $id_produk = substr(str_replace(".", "", microtime(true)), 8);
+        $getLastIdProduct = "SELECT * FROM produk ORDER BY id_produk DESC LIMIT 1";
+        $idProduct = $this->db->query($getLastIdProduct);
+        $resultIdProduct = $idProduct->fetchAll(PDO::FETCH_ASSOC);
 
-        $sql = "INSERT INTO `produk` (
+        $id_produk = $resultIdProduct[0]['id_produk'];
+
+        if($resultIdProduct == NULL){
+            $id_produk = 1;
+            $deskripsi = trim(preg_replace("/'/", " ",$data['deskripsi']));
+            $keterangan = trim(preg_replace("/'/", " ",$data['keterangan']));
+            $sql = "INSERT INTO `produk` (
                 `id_produk`, 
                 `id_toko`, 
-                `nama`
-            ) VALUES (
-                ".$id_produk.",
-                ".$id_toko.",
-                '".$data['name']."'
-            )";
-        $this->db->query($sql);
+                `nama`,
+                `deskripsi`,
+                `keterangan`
+                ) VALUES (
+                    $id_produk,
+                    $id_toko,
+                    '$data[nama]',
+                    '". $deskripsi ."',
+                    '". $keterangan ."'
+                )"; 
+            $this->db->query($sql);
+        }else{
+            $id_produk = $resultIdProduct[0]['id_produk'] + 1;
+            $sql = "INSERT INTO `produk` (
+                `id_produk`, 
+                `id_toko`, 
+                `nama`,
+                `deskripsi`,
+                `keterangan`
+                ) VALUES (
+                    $id_produk,
+                    $id_toko,
+                    '$data[nama]',
+                    '". $deskripsi ."',
+                    '". $keterangan ."'
+                )";
+            $this->db->query($sql);
+        }
+        
+        $getLastDataProduct = "SELECT * FROM data_produk ORDER BY id_data_produk DESC LIMIT 1";
+        $idDataProduct = $this->db->query($getLastDataProduct);
+        $resultDataIdProduct = $idDataProduct->fetchAll(PDO::FETCH_ASSOC);
 
-        $id_data = substr(str_replace(".", "", microtime(true)), 8);
+        $id_data_produk = $resultDataIdProduct[0]['id_data_produk'];
 
-        $sql = "INSERT INTO `data_produk`(
+        if($resultDataIdProduct == NULL){
+            $id_data_produk = 1;
+
+            $sql = "INSERT INTO `data_produk`(
                 `id_data_produk`, 
                 `id_produk`, 
+                `tgl_crawl`,
                 `rating`, 
                 `jml_review`, 
                 `diskon`, 
                 `harga`, 
                 `jml_barang`, 
-                `jml_terjual`
-            ) VALUES (
-                ".$id_data.",
-                ".$id_produk.",
-                ".$data['rating'].",
-                '".$data['review_count']."',
-                '".$data['discount_price']."',
-                '".$data['normal_price']."',
-                '".$data['stock']."',
-                '".$data['product_sold_count']."'
-            )";
+                `kondisi`, 
+                `jml_view`,
+                `waktu_proses`,
+                `jml_favorit`
+                ) VALUES (
+                    $id_data_produk,
+                    $id_produk,
+                    '$data[tanggal_crawl]',
+                    '$data[rating]',
+                    $data[jumlah_review],
+                    '$data[diskon]',
+                    '$data[harga]',
+                    $data[jumlah_barang],
+                    '$data[kondisi_barang]',
+                    $data[jumlah_view],
+                    '$data[waktu_proses]',
+                    $data[jumlah_favorit]
+                )";
         
-        $this->db->query($sql);
+            $tes = $this->db->query($sql);
+            var_dump($sql);
+        }else{
+            $id_data_produk = $resultDataIdProduct[0]['id_data_produk'] + 1;
+
+            $sql = "INSERT INTO `data_produk`(
+                `id_data_produk`, 
+                `id_produk`, 
+                `tgl_crawl`,
+                `rating`, 
+                `jml_review`, 
+                `diskon`, 
+                `harga`, 
+                `jml_barang`, 
+                `kondisi`, 
+                `jml_view`,
+                `waktu_proses`,
+                `jml_favorit`
+                ) VALUES (
+                    $id_data_produk,
+                    $id_produk,
+                    '$data[tanggal_crawl]',
+                    '$data[rating]',
+                    '$data[jumlah_review]',
+                    '$data[diskon]',
+                    '$data[harga]',
+                    '$data[jumlah_barang]',
+                    '$data[kondisi_barang]',
+                    '$data[jumlah_view]',
+                    '$data[waktu_proses]',
+                    '$data[jumlah_favorit]'
+                )";
+        
+            $this->db->query($sql);
+
+        }
+        
 
     }
 }
