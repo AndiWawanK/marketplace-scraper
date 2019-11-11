a0d92863ea24db92b4e34123a82cfab80d6740fb
Andi WawanK <andifirwansyah250@gmail.com>
2019-11-12 06:06:10 +0700

    call function to update perusahaan

--- index.php
+++ index.php
@@ -1,70 +1,71 @@
 <?php
 
 require_once "vendor/autoload.php";
-require_once __DIR__."/src/Marketplaces/Bukalapak.php";
-require_once __DIR__."/src/Marketplaces/Shopee.php";
-require_once __DIR__."/src/Marketplaces/Blanja.php";
+require_once __DIR__ . "/src/Marketplaces/Bukalapak.php";
+require_once __DIR__ . "/src/Marketplaces/Shopee.php";
+require_once __DIR__ . "/src/Marketplaces/Blanja.php";
 require_once __DIR__ . "/src/DB/Db.php";
 
 use Marketplaces\Bukalapak;
 use Marketplaces\Shopee;
-use Marketplaces\Blanja;
-
 
 $start_time = microtime(true);
 
 $marketplace = "bukalapak";
 
-$db = new DB('localhost:8889','esmart2','root','root');
+$db = new DB('localhost:8889', 'esmart2', 'root', 'root');
+
+
 $store = $db->getStore($marketplace);
 
 // $storeURL = [];
-while($stores = $store->fetch(PDO::FETCH_ASSOC)){
+while ($stores = $store->fetch(PDO::FETCH_ASSOC)) {
     // $storeURL[] = $stores['url'];
 
     $bukalapak = new Bukalapak($stores['url']);
     $store_info = $bukalapak->getStoreInformation();
-    if($store_info){
-        echo "Scrap Store Information Successful\n";
-        // var_dump($stores['url'], $stores['id_perusahaan']);
+    echo "Scrap Store Information Successful\n";
+    
+    if ($store_info) {
+        // var_dump($stores['id_perusahaan'],$stores['url']);
         $update_store = $db->updateStore($store_info, $stores['id_perusahaan']);
-        
-        // $store_products = $bukalapak->getProducts();
-        // var_dump($store_products);
-        // if($store_products){
-        //     echo "Scrap Product Store Successful\n";
-        // }else{
-        //     echo "Scrap Product Store Failed\n";
-        // }
+        echo "Update Store Information Successful \n";
 
-    }else{
+        $product_links = $bukalapak->Productlinks;
+
+        for ($i = 0; $i <= 1; $i++) {
+            echo "get Data " . $product_links[$i] . "\n";
+
+            if ($db->isExist($product_links[$i])) {
+                echo "Product already saved, Skip to next product \n";
+                continue;
+            }
+
+            $product = $bukalapak->getProduct($product_links[$i]);
+            
+            echo "Insert to DB \n";
+            
+            $db->insertProduct($product,$stores['id_toko']);
+            $db->addToSavedLink($product_links[$i]);
+        }
+
+    } else {
         echo "Scrap Store Information Failed\n";
     }
 
 }
 // foreach($storeURL as $url){
-    
-    
+
 // }
 // $bukalapak = new Bukalapak("https://www.bukalapak.com/u/rikky");
 
 // $store_info = $bukalapak->getStoreInformation();
 // $store_products = $bukalapak->getProducts();
 
-// 
+//
 
 // $id_toko = $db->insertStore($store_info);
 // $insert_product = $db->insertProducts($store_products, $id_toko);
-
-if($insert_product){
-    echo "Insert Data Success! \n";
-}else{
-    echo "Failed to Insert Data! \n";
-}
-
-
-
-
 
 // // End clock time in seconds
 $end_time = microtime(true);
